60a4377b0ebade9ee05d8ec7d12bac048f55109c
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-18 22:50:12 +0300

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 //import logo from './logo.svg';//
 import React from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This Coded by
+        <a
+          href="https://github.com/SheillaO"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Olga
+        </a>
+        and is
+        <a
+          href="https://github.com/SheillaO/weather-forecast-react-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        and
+        <a
+          href="https://weather-forecast-react-app-dashboard.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
-
-
