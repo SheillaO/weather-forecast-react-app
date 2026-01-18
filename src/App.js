737cce89d3caddadefbadef757df488c1f38bc31
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-18 22:58:01 +0300

    Updated the footer

--- src/App.js
+++ src/App.js
@@ -7,33 +7,32 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This Coded by
-        <a
-          href="https://github.com/SheillaO"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Olga
-        </a>
+  This is coded by{" "}
+  <a
+    href="https://github.com/SheillaO"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Olga
+  </a>{" "}
+  and is{" "}
+  <a
+    href="https://github.com/SheillaO/weather-forecast-react-app"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    open-sourced on GitHub
+  </a>{" "}
+  and{" "}
+  <a
+    href="https://weather-forecast-react-app-dashboard.netlify.app/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    hosted on Netlify
+  </a>
+</footer>
 
-        and is
-        <a
-          href="https://github.com/SheillaO/weather-forecast-react-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        
-        and
-        <a
-          href="https://weather-forecast-react-app-dashboard.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
     </div>
   );
 }
