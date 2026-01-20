7e31d4cd90a85a2c90d207ce6cf543a4e9573f57
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-20 11:20:50 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-  This is coded by{" "}
-  <a
-    href="https://github.com/SheillaO"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Olga
-  </a>{" "}
-  and is{" "}
-  <a
-    href="https://github.com/SheillaO/weather-forecast-react-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    open-sourced on GitHub
-  </a>{" "}
-  and{" "}
-  <a
-    href="https://weather-forecast-react-app-dashboard.netlify.app/"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    hosted on Netlify
-  </a>
-</footer>
-
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This is coded by{" "}
+          <a
+            href="https://github.com/SheillaO"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Olga
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/SheillaO/weather-forecast-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weather-forecast-react-app-dashboard.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
