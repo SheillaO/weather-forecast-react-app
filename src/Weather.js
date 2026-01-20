2c6c74277a4097ad1f93cf898b149caada675e21
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-20 13:18:58 +0300

    Worked on the CSS of the weather project

--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,53 @@
 import React from "react";
 import "./Weather.css";
 
-export default function Weather(){
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city .."
-                className="form-control"
-              />
-            </div>
-            <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary" />
-            </div>
-          </div>
-        </form>
-        <h1>New York</h1>
-        <ul>
-          <li>Wednesday 07:00</li>
-          <li>Mostly Cloudy</li>
-        </ul>
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
         <div className="row">
-          <div className="col-6">
-            <img
-              src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
-              alt="Mostly cloudy"
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city .."
+              className="form-control"
+              autoFocus="on"
             />
-            24 °C
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 10% </li>
-              <li>Humidity: 44% </li>
-              <li>Wind: 21 km/h</li>
-            </ul>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
+            alt="Mostly cloudy"
+            className="float-left"
+          />
+          <div className="float-left">
+            <span className="temperature">24</span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 10% </li>
+            <li>Humidity: 44% </li>
+            <li>Wind: 21 km/h</li>
+          </ul>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
