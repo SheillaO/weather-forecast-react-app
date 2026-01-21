ff5d92bc532943330a4c5fad01f5ffaf731b8c9f
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-21 12:14:09 +0300

    Worked on customized weather icons, and css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,35 +1,37 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "/WeatherIcon";
 
-export default function WeatherInfo(props){
-    return (
-      <div className="WeatherInfo">
-        <h1>{props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <img
-              src={weatherData.iconurl}
-              alt={weatherData.description}
-              className="float-left"
-            />
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={weatherData.date} />
+        </li>
+        <li className="text-capitalize">{weatherData.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
             <div className="float-left">
               <span className="temperature">{Math.round(temperature)}</span>
               <span className="unit">°C</span>
             </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity:{weatherData.humidity}%</li>
-              <li>Wind:{weatherData.wind}km/h</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity:{weatherData.humidity}%</li>
+            <li>Wind:{weatherData.wind}km/h</li>
+          </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
