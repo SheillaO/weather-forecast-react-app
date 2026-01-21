891909261f54466f03e90e4c31051f2c1ce05d4f
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-21 15:39:07 +0300

    Looped the weather forecast through an array

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,34 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props){
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] =useState(null);
 
     function handleResponse(response){
-        console.log(response.data);
+        setForecast(response.data.daily)
+        setLoaded(true);
     }
     console.log(props);
 
-    let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://ap.penweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    
 
-    axios.get(apiUrl).then(handleResponse);
-
+    if (loaded) {
+        return (
+          <div className="WeatherForecast">
+            <div className="row">
+{forecast.map(function(dailyForecast, index) {
+  if (index < 5) {
     return (
-      <div className="WeatherForecast">
-        <div className="row">
-          <div className="col">
-            <div className="WeatherForecast-day">Thu</div>
-            <WeatherIcon code="01d" size={36} />
-            <div className="WeatherForecast-temperatures">
-              <span className="WeatherForecast-temperature-max">19°C</span>
-              <span className="WeatherForecast-temperature-min">10°C</span>
-            </div>
-          </div>
-        </div>
+      <div className="col" key={index}>
+        <WeatherForecastDay data={dailyForecast} />
       </div>
     );
+  }
+}
+)}  
+            </div>
+          </div>
+        );
+    } else {
+        let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://ap.penweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+
+        return null;
+    }
 }
