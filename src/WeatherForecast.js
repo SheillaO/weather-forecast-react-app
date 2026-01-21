a1eb53943c04ba582b755d67330068773b3f3413
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-21 14:47:04 +0300

    Worked on the weather forecast api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,22 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast(){
+export default function WeatherForecast(props){
+
+    function handleResponse(response){
+        console.log(response.data);
+    }
+    console.log(props);
+
+    let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://ap.penweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
     return (
       <div className="WeatherForecast">
         <div className="row">
@@ -12,7 +26,6 @@ export default function WeatherForecast(){
             <div className="WeatherForecast-temperatures">
               <span className="WeatherForecast-temperature-max">19°C</span>
               <span className="WeatherForecast-temperature-min">10°C</span>
-              19 10
             </div>
           </div>
         </div>
