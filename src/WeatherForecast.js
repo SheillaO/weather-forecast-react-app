e4efa55c49e49e0b75a22295755e7c059e6510de
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-21 16:06:59 +0300

    Updated the weather.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect} from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
-import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
@@ -32,7 +30,10 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else{
+                return null;
             }
+
           })}
         </div>
       </div>
