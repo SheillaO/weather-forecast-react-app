f6725319c9abbdb58c553080c3e200d76ae978db
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-21 17:08:23 +0300

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,12 +3,12 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.data.temp.max);
+    let temperature = Math.round(props.data.main.temp.max);
     return `${temperature}°`;
   }
 
   function minTemperature() {
-    let temperature = Math.round(props.data.temp.min);
+    let temperature = Math.round(props.data.main.temp.min);
     return `${temperature}°`;
   }
 
